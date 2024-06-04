9b6c1b15ab7e2f062cce8890f5f3a63fe9cdbe8d
Olaoluwa Osuntokun <laolu32@gmail.com>
2024-06-03 23:06:17 -0700

    sweep: add new AuxSweeper interface
    In this commit, we add a new AuxSweeper interface. This'll take a set of
    inputs, and a change addr for the sweep transaction, then optionally
    return a new sweep output to be added to the sweep transaction.
    
    We also add a new NotifyBroadcast method.  This'll be used to notify
    that we're _about_ to broadcast a sweeping transaction. The set of
    inputs is passed in, which allows the caller to prepare for the ultimate
    broadcast of the sweeping transaction.
    
    We also add ExtraTxOut to BumpRequest pass fees to NotifyBroadcast. This
    allows the callee to know the total fee of the sweeping transaction.

--- server.go
+++ server.go
@@ -4900,12 +4900,7 @@ func newSweepPkScriptGen(
 
 		return fn.Ok(lnwallet.AddrWithKey{
 			DeliveryAddress: addr,
-			InternalKey: fn.MapOption(func(
-				desc keychain.KeyDescriptor) btcec.PublicKey {
-
-				return *desc.PubKey
-			},
-			)(internalKeyDesc),
+			InternalKey:     internalKeyDesc,
 		})
 	}
 }
